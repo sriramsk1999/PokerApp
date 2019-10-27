7fab158974462605969629b25e8214a3708859e3
Sriram <sriramsk1999@gmail.com>
2019-10-27 16:25:20 +0530

    Added folder for images

--- poker_new.py
+++ poker_new.py
@@ -12,22 +12,22 @@ def table_cards(window):
 
     #initial 3 cards to display.
     global card_1_image,card_2_image,card_3_image,card_4_image,card_river_image
-    card_1_image = Image.open("card.jpg")
+    card_1_image = Image.open("images/card.jpg")
     card_1_image = card_1_image.resize((70, 80), Image.ANTIALIAS)
     card_1_image = ImageTk.PhotoImage(card_1_image)
     card_1_image_label = Label(window, image = card_1_image, width =60, height = 70) 
     #card_1_image_label.place(x = 450, y = 250)
 
 
-    # card_2_image = ImageTk.PhotoImage(Image.open("card.jpg"))
-    card_2_image = Image.open("card.jpg")
+    # card_2_image = ImageTk.PhotoImage(Image.open("images/card.jpg"))
+    card_2_image = Image.open("images/card.jpg")
     card_2_image = card_2_image.resize((70, 80), Image.ANTIALIAS)
     card_2_image = ImageTk.PhotoImage(card_2_image)
     card_2_image_label = Label(window, image = card_2_image, width =60, height = 70) 
     card_2_image_label.place(x = 520, y = 250)
 
-    # card_3_image = ImageTk.PhotoImage(Image.open("card.jpg"))
-    card_3_image = Image.open("card.jpg")
+    # card_3_image = ImageTk.PhotoImage(Image.open("images/card.jpg"))
+    card_3_image = Image.open("images/card.jpg")
     card_3_image = card_3_image.resize((70, 80), Image.ANTIALIAS)
     card_3_image = ImageTk.PhotoImage(card_3_image)
     card_3_image_label = Label(window, image = card_3_image, width =60, height = 70) 
@@ -35,16 +35,16 @@ def table_cards(window):
 
 #if condition to display according to Poker rules.
 
-    # card_4_image = ImageTk.PhotoImage(Image.open("card.jpg"))
-    card_4_image = Image.open("card.jpg")
+    # card_4_image = ImageTk.PhotoImage(Image.open("images/card.jpg"))
+    card_4_image = Image.open("images/card.jpg")
     card_4_image = card_4_image.resize((70, 80), Image.ANTIALIAS)
     card_4_image = ImageTk.PhotoImage(card_4_image)
     card_4_image_label = Label(window, image = card_4_image, width =60, height = 70) 
     card_4_image_label.place(x = 660, y = 250)    
 
 
-    # card_river_image = ImageTk.PhotoImage(Image.open("card.jpg"))
-    card_river_image = Image.open("card.jpg")
+    # card_river_image = ImageTk.PhotoImage(Image.open("images/card.jpg"))
+    card_river_image = Image.open("images/card.jpg")
     card_river_image = card_river_image.resize((70, 80), Image.ANTIALIAS)
     card_river_image = ImageTk.PhotoImage(card_river_image)
     card_river_image_label = Label(window, image = card_river_image, width =60, height = 70) 
@@ -70,12 +70,12 @@ def players(window):
     player1_name.pack(side = "bottom") 
 
 
-    player1_card_1_image = Image.open("card.jpg")
+    player1_card_1_image = Image.open("images/card.jpg")
     player1_card_1_image = player1_card_1_image.resize((40, 50), Image.ANTIALIAS)
     player1_card_1_image = ImageTk.PhotoImage(player1_card_1_image)
     player1_card_1_image_label = Label(window, image = player1_card_1_image, width =30, height = 40) 
     player1_card_1_image_label.place(x = 640, y = 550)
-    player1_card_2_image = Image.open("card.jpg")
+    player1_card_2_image = Image.open("images/card.jpg")
     player1_card_2_image = player1_card_2_image.resize((40, 50), Image.ANTIALIAS)
     player1_card_2_image = ImageTk.PhotoImage(player1_card_2_image)
     player1_card_2_image_label = Label(window, image = player1_card_2_image, width =30, height = 40) 
@@ -93,12 +93,12 @@ def players(window):
     player2_name.place(x = 0, y = 470)
 
 
-    player2_card_1_image = Image.open("card.jpg")
+    player2_card_1_image = Image.open("images/card.jpg")
     player2_card_1_image = player2_card_1_image.resize((40, 50), Image.ANTIALIAS)
     player2_card_1_image = ImageTk.PhotoImage(player2_card_1_image)
     player2_card_1_image_label = Label(window, image = player2_card_1_image, width =30, height = 40) 
     player2_card_1_image_label.place(x = 5, y = 420)
-    player2_card_2_image = Image.open("card.jpg")
+    player2_card_2_image = Image.open("images/card.jpg")
     player2_card_2_image = player2_card_2_image.resize((40, 50), Image.ANTIALIAS)
     player2_card_2_image = ImageTk.PhotoImage(player2_card_2_image)
     player2_card_2_image_label = Label(window, image = player2_card_2_image, width =30, height = 40) 
@@ -116,12 +116,12 @@ def players(window):
     player5_name.place(x = 1120, y = 470)
 
 
-    player5_card_1_image = Image.open("card.jpg")
+    player5_card_1_image = Image.open("images/card.jpg")
     player5_card_1_image = player5_card_1_image.resize((40, 50), Image.ANTIALIAS)
     player5_card_1_image = ImageTk.PhotoImage(player5_card_1_image)
     player5_card_1_image_label = Label(window, image = player5_card_1_image, width =30, height = 40) 
     player5_card_1_image_label.place(x = 1120, y = 420)
-    player5_card_2_image = Image.open("card.jpg")
+    player5_card_2_image = Image.open("images/card.jpg")
     player5_card_2_image = player5_card_2_image.resize((40, 50), Image.ANTIALIAS)
     player5_card_2_image = ImageTk.PhotoImage(player5_card_2_image)
     player5_card_2_image_label = Label(window, image = player5_card_2_image, width =30, height = 40) 
@@ -139,12 +139,12 @@ def players(window):
     player3_name.place(x = 0, y = 90)
 
 
-    player3_card_1_image = Image.open("card.jpg")
+    player3_card_1_image = Image.open("images/card.jpg")
     player3_card_1_image = player3_card_1_image.resize((40, 50), Image.ANTIALIAS)
     player3_card_1_image = ImageTk.PhotoImage(player3_card_1_image)
     player3_card_1_image_label = Label(window, image = player3_card_1_image, width =30, height = 40) 
     player3_card_1_image_label.place(x = 5, y = 40)
-    player3_card_2_image = Image.open("card.jpg")
+    player3_card_2_image = Image.open("images/card.jpg")
     player3_card_2_image = player3_card_2_image.resize((40, 50), Image.ANTIALIAS)
     player3_card_2_image = ImageTk.PhotoImage(player3_card_2_image)
     player3_card_2_image_label = Label(window, image = player3_card_2_image, width =30, height = 40) 
@@ -162,12 +162,12 @@ def players(window):
     player4_name.place(x = 1120, y = 90)
     
    
-    player4_card_1_image = Image.open("card.jpg")
+    player4_card_1_image = Image.open("images/card.jpg")
     player4_card_1_image = player4_card_1_image.resize((40, 50), Image.ANTIALIAS)
     player4_card_1_image = ImageTk.PhotoImage(player4_card_1_image)
     player4_card_1_image_label = Label(window, image = player4_card_1_image, width =30, height = 40) 
     player4_card_1_image_label.place(x = 1120, y = 40)
-    player4_card_2_image = Image.open("card.jpg")
+    player4_card_2_image = Image.open("images/card.jpg")
     player4_card_2_image = player4_card_2_image.resize((40, 50), Image.ANTIALIAS)
     player4_card_2_image = ImageTk.PhotoImage(player4_card_2_image)
     player4_card_2_image_label = Label(window, image = player4_card_2_image, width =30, height = 40) 
@@ -180,7 +180,7 @@ def main():
     window = Tk()
     window.title("Poker")
     window.configure(background = "black")
-    center_name = Label(window, text = "Payer_name's turn", bg="black", fg="white", font = "Helvetica 30 bold")
+    center_name = Label(window, text = "Player_name's turn", bg="black", fg="white", font = "Helvetica 30 bold")
     center_name.pack(side="top")
     table(window)
     players(window)
