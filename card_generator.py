950cd9bc184bfbf404d11e6d298c65e846528a8c
Sriram <sriramsk1999@gmail.com>
2019-10-31 17:47:26 +0530

    Added Hand Evaluator

--- card_generator.py
+++ card_generator.py
@@ -1,37 +1,70 @@
 import random
 import pprint
+from poker_data import *
+import itertools
+from copy import deepcopy
+
+_SUITS = [1 << (i + 12) for i in range(4)]
+_RANKS = [(1 << (i + 16)) | (i << 8) for i in range(13)]
+_PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+_DECK = [_RANKS[rank] | _SUITS[suit] | _PRIMES[rank] for rank, suit in 
+    itertools.product(range(13), range(4))]
+
+SUITS = 'cdhs'
+RANKS = '23456789TJQKA'
+DECK = [''.join(s) for s in itertools.product(RANKS, SUITS)]
+LOOKUP = dict(zip(DECK, _DECK))
 
 def gen_game(nplayers): # readies cards for players and table
     player = [[] for i in range(nplayers)]
     table = []
-        
-    for i in range(nplayers): #number of players
-        while(len(player[i])!=3): #3 cards for each playerstr(num[random.randint(0,12)]) 
-            card = str(num[random.randint(0,12)]) + ' of ' + suit[random.randint(0,3)]
-            if(card in allcards):
-                player[i].append(card)
-                allcards.remove(card)
-        while(len(table)!=5):
-            card = str(num[random.randint(0,12)]) + ' of ' + suit[random.randint(0,3)]
-            if(card in allcards):
-                table.append(card)
-                allcards.remove(card)
+    deck=list(DECK)
+    random.shuffle(deck)
+    table=deck[:5]
+    for i in range(nplayers):
+        player[i].append(deck[5+i])
+    for i in range(nplayers):
+        player[i].append(deck[5+nplayers+i])
     return player,table
 
 def winner(player,table):
-    #To-Do
-    pass
+    scores=[]
+    hands=deepcopy(player)
+    table1=deepcopy(table)
+    for i in range(len(hands)):
+        hands[i].extend(table1)
+        score=eval7(hands[i])
+        scores.append(score)
+    print("player",scores.index(min(scores))+1,"is the winner")
+    return scores.index(max(scores))
 
-global allcards
-num = list(range(1,11))
-num[0] = 'ace';
-num.extend(['jack','queen','king'])
-suit = ['clubs','spades','diamonds','hearts']
-allcards = [(i,j) for i in suit for j in num]
+def hash_function(x):
+    x += 0xe91aaa35
+    x ^= x >> 16
+    x += x << 8
+    x &= 0xffffffff
+    x ^= x >> 4
+    b = (x >> 8) & 0x1ff
+    a = (x + (x << 2)) >> 19
+    r = (a ^ HASH_ADJUST[b]) & 0x1fff
+    return HASH_VALUES[r]
+
+def eval5(hand):
+    c1, c2, c3, c4, c5 = (LOOKUP[x] for x in hand)
+    q = (c1 | c2 | c3 | c4 | c5) >> 16
+    if (0xf000 & c1 & c2 & c3 & c4 & c5):
+        return FLUSHES[q]
+    s = UNIQUE_5[q]
+    if s:
+        return s
+    p = (c1 & 0xff) * (c2 & 0xff) * (c3 & 0xff) * (c4 & 0xff) * (c5 & 0xff)
+    return hash_function(p)
+
+def eval7(hand):
+    return min(eval5(x) for x in itertools.combinations(hand, 5))
 
 nplayers=int(input())
 player,table = gen_game(nplayers)    
 pprint.pprint(player)
 pprint.pprint(table)
 winner(player,table)
-       
